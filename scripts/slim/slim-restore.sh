7db2afc5e7aa6f1627f7c6f2f243ab8384af11f4
Travis Gall <travis@rnengineering.com>
2016-05-17 13:19:03 -0600

    issue-#1: Moved all existing files from arch-linux:master into this repository

---
+++ scripts/slim/slim-restore.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/Documents/arch-linux/slim/config/.xinitrc ~/.xinitrc
+cp ~/Documents/arch-linux/slim/config/slim.conf /etc/slim.conf
+cp ~/Documents/arch-linux/slim/config/slimlock.conf /etc/slimlock.conf

c3703d9065797119edf2f8c65a756bf10c164943
Travis Gall <travis.gall@gmail.com>
2016-05-20 09:24:40 -0600

    #27: Moved non-critical apps to separate script

---
+++ scripts/arch/app-install.sh
@@ -0,0 +1,18 @@
+
+# Install custom tools
+arch-chroot /mnt pacman -S --noconfirm baobab               # Disk usage analyzer
+arch-chroot /mnt pacman -S --noconfirm clementine           # Music player
+#arch-chroot /mnt pacman -S --noconfirm conky               # System monitor (side panel)
+arch-chroot /mnt pacman -S --noconfirm deluge               # Bit torrent client
+arch-chroot /mnt pacman -S --noconfirm geany                # Text editor
+arch-chroot /mnt pacman -S --noconfirm gnome-calculator     # Simple calcuator
+arch-chroot /mnt pacman -S --noconfirm gnome-system-monitor # System monitor
+arch-chroot /mnt pacman -S --noconfirm gparted              # Disk partition manager
+arch-chroot /mnt pacman -S --noconfirm libreoffice          # Office documents
+arch-chroot /mnt pacman -S --noconfirm remmina freerdp      # Remote desktop connection manager
+#arch-chroot /mnt pacman -S --noconfirm teamviewer          # Remote connections
+arch-chroot /mnt pacman -S --noconfirm thunderbird          # Email client (replace with evolution)
+arch-chroot /mnt pacman -S --noconfirm virtualbox           # System virtualization
+arch-chroot /mnt pacman -S --noconfirm vlc                  # Media player
+#arch-chroot /mnt pacman -S --noconfirm wine                # Windows "emulator"
+arch-chroot /mnt pacman -S --noconfirm xfcef-notifyd		# Notification handler for xfce4
--- scripts/arch/arch-install.sh
+++ scripts/arch/arch-install.sh
@@ -121,30 +121,13 @@ arch-chroot echo "$nameUser ALL=(ALL) ALL" >> /mnt/etc/sudoers
 arch-chroot /mnt pacman -S --noconfirm curlftpfs																						# Install
 echo "curlftpfs#rgretchen:Cycology1@ftp.cycologybikes.ca /mnt/cycology-ftp fuse auto,user,allow_other,_netdev 0 0" >> /mnt/etc/fstab	# Configure fstab
 
-# Install custom tools
-arch-chroot /mnt pacman -S --noconfirm baobab               # Disk usage analyzer
-arch-chroot /mnt pacman -S --noconfirm clementine           # Music player
-#arch-chroot /mnt pacman -S --noconfirm conky               # System monitor (side panel)
-arch-chroot /mnt pacman -S --noconfirm deluge               # Bit torrent client
-arch-chroot /mnt pacman -S --noconfirm firefox              # Web browser
-arch-chroot /mnt pacman -S --noconfirm geany                # Text editor
-arch-chroot /mnt pacman -S --noconfirm gnome-calculator     # Simple calcuator
-arch-chroot /mnt pacman -S --noconfirm gnome-system-monitor # System monitor
-arch-chroot /mnt pacman -S --noconfirm gparted              # Disk partition manager
-arch-chroot /mnt pacman -S --noconfirm libreoffice          # Office documents
-arch-chroot /mnt pacman -S --noconfirm openssh              # Secure shell
-arch-chroot /mnt pacman -S --noconfirm remmina freerdp      # Remote desktop connection manager
-arch-chroot /mnt pacman -S --noconfirm rsync                # Folder synchronization
-arch-chroot /mnt pacman -S --noconfirm samba                # Windows network shares
-arch-chroot /mnt pacman -S --noconfirm sshfs                # Secure shell file system
-#arch-chroot /mnt pacman -S --noconfirm teamviewer          # Remote connections
-arch-chroot /mnt pacman -S --noconfirm thunderbird          # Email client (replace with evolution)
-arch-chroot /mnt pacman -S --noconfirm terminator		# Working terminal
-arch-chroot /mnt pacman -S --noconfirm virtualbox           # System virtualization
-arch-chroot /mnt pacman -S --noconfirm vlc                  # Media player
-arch-chroot /mnt pacman -S --noconfirm wget					# Non-interactive network downloader
-#arch-chroot /mnt pacman -S --noconfirm wine                # Windows "emulator"
-arch-chroot /mnt pacman -S --noconfirm xfcef-notifyd		# Notification handler for xfce4
+arch-chroot /mnt pacman -S --noconfirm firefox		# Web browser
+arch-chroot /mnt pacman -S --noconfirm openssh		# Secure shell
+arch-chroot /mnt pacman -S --noconfirm rsync		# Folder synchronization
+arch-chroot /mnt pacman -S --noconfirm samba		# Windows network shares
+arch-chroot /mnt pacman -S --noconfirm sshfs		# Secure shell file system
+arch-chroot /mnt pacman -S --noconfirm terminator	# Working terminal
+arch-chroot /mnt pacman -S --noconfirm wget		# Non-interactive network downloader
 
 ########################################################################
 # Bootloader

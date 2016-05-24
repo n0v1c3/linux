bb0247a9007200407928c99dd442ca4f7dcb979e
Travis Gall <travis.gall@gmail.com>
2016-05-24 06:54:43 -0600

    #37: + vimrc config and setup

--- scripts/vim/setup.sh
+++ scripts/vim/setup.sh
@@ -2,8 +2,14 @@
 
 key_binding_file="/usr/share/X11/xkd/symbols/ca"
 
-# Buck-up existing key binding to be overwritten
-mv "$key_binding_file" "$keybinding-backup"
+#Back-up existing key binding to be overwritten
+#cp "$key_binding_file" "$keybinding-backup"
 
 # Copy custom key binding into the desired language
-cp ~/Documents/linux/config/vim/ca "$key_binding_file"
+#cp ~/Documents/linux/config/vim/ca "$key_binding_file"
+
+# Backup current vimrc
+cp ~/.vimrc ~/.vimrc-backup
+
+# Replace current vimrc with custom vimrc from repository
+cp ~/Documents/linux/config/vim/vimrc ~/.vimrc

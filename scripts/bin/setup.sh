46564ed6fb3395f45631437b601755fe31346d5c
Travis Gall <travis.gall@gmail.com>
2016-05-25 22:37:27 -0600

    #34: Create installer for user bin folder

---
+++ scripts/bin/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Backkup user's existing .bin folder
+cp -r ~/.bin ~/.bin-backup
+
+# Create .bin folder if none exists
+mkdir ~/.bin
+
+# Add path to .bashrc file
+bashrc_path="export PATH=\$PATH:~/.bin"
+count=$(cat ~/.bashrc | grep "$bashrc_path" | wc -l)
+echo $count
+if [ $count -eq 0 ]; then
+	echo "$bashrc_path" >> ~/.bashrc
+fi
+
+# Create links to desired scrpits
+ln -s ~/Documents/linux/scripts/bash/c.sh ~/.bin/c
+ln -s ~/Documents/linux/scripts/bash/ca.sh ~/.bin/ca
+ln -s ~/Documents/linux/scritps/tmux/my-tmux.sh ~/.bin/my-tmux

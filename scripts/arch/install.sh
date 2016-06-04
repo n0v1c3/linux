88a3887bee0146583584b00515ff60fe589d8a5b
Travis Gall <travis.gall@gmail.com>
2016-06-04 14:35:20 -0600

    #43: + repo-dir to arch/install.sh

--- scripts/arch/install.sh
+++ scripts/arch/install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 source repo-dir.sh
-echo $(repoDir)
-
-repo_dr="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-length=`echo $repo_dir | grep -b -o "/linux/" | awk 'BEGIN {FS=":"}{print $1}'`
-parent_dir=${repo_dir:0:$length}"/linux"
+parent_dir=echo $(repoDir)
 
 echo -e "Please enter a hostname: "
 read nameHost

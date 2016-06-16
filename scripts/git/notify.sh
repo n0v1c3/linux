c8b4e9b7e07852fb04c670ef12c4a95097c27c0b
Travis Gall <travis.gall@gmail.com>
2016-06-16 16:38:51 -0600

    Remove flicker from git/notify

--- scripts/git/notify.sh
+++ scripts/git/notify.sh
@@ -1,59 +1,40 @@
 #!/bin/bash
 
 gitdir="$(pwd)/.git"
-
-# Titles
 title="$(basename `git --git-dir=$gitdir rev-parse --show-toplevel`)"
-titleWidth=$((${#title} + 2))
-title1="BRANCH"
-title2="STATUS"
-title3="LOG"
 
-# Font colours
-NC='\033[0m'		# No colour
-PURPLE='\033[1;35m'
-WHITE='\033[1;37m'
+NC='\033[0m'
+BOLD='\033[1;37m'
 
-while true; do
-	clear
+git-notify() {
+	while true; do
+		current_status=$(git --git-dir=$gitdir status -sb)
 
-	# Branch title
-	printf "${PURPLE}"
-	printf "╔"
-	printf "%0.s═" $(seq 1 $titleWidth)
-	printf "╗"
-	echo ""
-	printf "║ $title ║\n"
-	printf "╚"
-	printf "%0.s═" $(seq 1 $titleWidth)
-	printf "╝"
-	echo ""
-	printf "${NC}"
-	
-	# Branch list
-	echo ""
-	printf "${WHITE}$title1${NC}\n"
-	git --git-dir=$gitdir branch
-
-	# Current status
-	echo ""
-	printf "${WHITE}$title2${NC}\n"
-	git --git-dir=$gitdir status -sb
-
-	# Log
-	echo ""
-	printf "${WHITE}$title3${NC}\n"
-	git --no-pager --git-dir=$gitdir log --pretty=format:'%s' -n 10 | cut -c1-44
-
-	sleep 3
-
-	if read -t0; then
-		read a
-
-		if [[ "$a" == "q" ]]; then
+		if [ "$current_status" != "$previous_status" ]; then
 			clear
-			break
+			printf "${BOLD}$title${NC}\n"
+			printf "\n${BOLD}BRANCH${NC}\n"
+			git --git-dir=$gitdir branch
+			printf "\n${BOLD}STATUS${NC}\n"
+			git --git-dir=$gitdir status -sb
+			printf "\n${BOLD}LOG${NC}\n"
+			git --no-pager --git-dir=$gitdir log --pretty=format:'%s' -n 10 | cut -c1-44
 		fi
-		git checkout $a
-	fi
-done
+
+		previous_status=$current_status
+
+		if read -t0; then
+			read a
+
+			if [[ "$a" == "q" ]]; then
+				clear
+			break
+			fi
+			git --git-dir=$gitdir checkout $a
+		fi
+	
+		sleep 1
+	done
+}
+
+git-notify

6b6d428413f5f8f9528aab271433dca5527ba599
Travis Gall <travis.gall@gmail.com>
2016-06-22 22:15:57 -0600

    #74: + confirm git pull status

--- scripts/git/notify.sh
+++ scripts/git/notify.sh
@@ -7,40 +7,44 @@ NC='\033[0m'
 BOLD='\033[1;37m'
 
 git-notify() {
-	while true; do
-		current_branch=$(git --git-dir=$gitdir branch)		
-		current_status=$(git --git-dir=$gitdir status -sb)
-		current_log=$(git --git-dir=$gitdir log --pretty=format:'%s' -n 1)
+while true; do
+	current_branch=$(git --git-dir=$gitdir branch)		
+	current_status=$(git --git-dir=$gitdir status -sb)
+	current_log=$(git --git-dir=$gitdir log --pretty=format:'%s' -n 1)
 
-		if [[ "$current_branch" != "$previous_branch" ]] || [[ "$current_status" != "$previous_status" ]] || [[ "$current_log" != "$previous_log" ]] ; then
+	if [[ "$current_branch" != "$previous_branch" ]] || [[ "$current_status" != "$previous_status" ]] || [[ "$current_log" != "$previous_log" ]] ; then
+		clear
+		printf "${BOLD}$title${NC}\n"
+		printf "\n${BOLD}BRANCH${NC}\n"
+		git --git-dir=$gitdir branch
+		printf "\n${BOLD}STATUS${NC}\n"
+		git --git-dir=$gitdir status -sb
+		printf "\n${BOLD}LOG${NC}\n"
+		git --no-pager --git-dir=$gitdir log --pretty=format:'%s' -n 10 | cut -c1-44
+
+		printf "\ngit checkout "
+	fi
+
+	previous_branch=$current_branch
+	previous_status=$current_status
+	previous_log=$current_log
+
+	# Read user input
+	if read -t0; then
+		read a
+
+		if [[ "$a" == "q" ]]; then
 			clear
-			printf "${BOLD}$title${NC}\n"
-			printf "\n${BOLD}BRANCH${NC}\n"
-			git --git-dir=$gitdir branch
-			printf "\n${BOLD}STATUS${NC}\n"
-			git --git-dir=$gitdir status -sb
-			printf "\n${BOLD}LOG${NC}\n"
-			git --no-pager --git-dir=$gitdir log --pretty=format:'%s' -n 10 | cut -c1-44
-			
-			printf "\ngit checkout "
-		fi
-
-		previous_branch=$current_branch
-		previous_status=$current_status
-		previous_log=$current_log
-
-		if read -t0; then
-			read a
-
-			if [[ "$a" == "q" ]]; then
-				clear
 			break
-			fi
-			git --git-dir=$gitdir checkout $a
 		fi
-	
-		sleep 1
-	done
+		git --git-dir=$gitdir checkout $a
+	fi
+
+	sleep 1
+done
 }
 
-git-notify
+if git pull; then
+	sleep 1
+	git-notify
+fi

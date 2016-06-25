2c65070ba0881a65f4b64183d42217964eb727ef
Travis Gall <travis.gall@gmail.com>
2016-06-25 08:08:52 +0000

    #27: Wrap-up

--- scripts/install/linux.sh
+++ scripts/install/linux.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-# 1 - Detect current os
-# 2 - Partition and format desired drive
-# 3 - Configure default network configuration
-# 4 - Create user
-# 5 - Configure clock
-# 6 - Configure language
+##
+# Read
+##
 
 # Get directory of the current bash script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# 2
-# Partition disk (disk to be selected by user and should be known in advance)
-# The new ext4 file system will be mounted directly on /mnt
+# Get user's information
 echo -n "Enter disk path (/dev/sd[x]): "
 read diskpath
-source $DIR/disk-partition.sh $diskpath
-
-# 3
 echo -n "Enter hostname: "
 read hostname
-
-# 4
+echo -n "Enter root password: "
+read root_pass
 echo -n "Enter username: "
 read user_name
 echo -n "Enter password: "
@@ -31,7 +23,6 @@ read user_full
 echo -n "Enter email: "
 read user_email
 
-# 1
 # Set parameters based on the current operating system
 case $(uname -a | tr '[:upper:]' '[:lower:]') in
 
@@ -58,36 +49,62 @@ case $(uname -a | tr '[:upper:]' '[:lower:]') in
 		;;
 esac
 
-# Install base kernel
-$base_install
-
 # Install command to be used when installing new software
 install_cmd="$sudo $installer"
 
-# 5
-# Set timezone
+##
+# Disks
+##
+
+# Partition (100M Grub | 2G Swap | x ext)
+echo -e "o\nn\np\n1\n\n+100M\nn\np\n2\n\n+2G\nn\np\n3\n\n\na\n1\nt\n2\n82\nw\n" | fdisk ${diskpath}
+
+# Swap file system
+mkswap ${diskpath}2
+swapon ${diskpath}2
+
+# Create ext4 file system
+mkfs.ext4 ${diskpath}3
+
+# Mount new file system
+mount ${diskpath}3 /mnt
+
+##
+# Base
+##
+
+# Kernel
+$base_install
+
+# Timezone
 $sudo ln -s /usr/share/zoneinfo/Canada/Mountain /mnt/etc/localtime
 
-# 6
 # Configure locales
 echo -e "en_US.UTF-8 UTF-8\nen_US ISO-8859-1" > /mnt/etc/locale.gen
-arch-chroot /mnt locale-gen
+$sudo locale-gen
 echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
 
-# Grub - Bootloader
+##
+# Network
+##
+
+# Generate fstab
+genfstab -p /mnt >> /mnt/etc/fstab
+
+# Set hostname
+echo $hostname > /mnt/etc/hostname
+
+# Enable DHCP
+arch-chroot /mnt systemctl enable dhcpcd.service
+
+##
+# Install
+##
+
+# Grub
 $install_cmd grub
-$sudo grub-install --target=i386-pc ${diskpath}
-$sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-# User - Add user and set password
-$sudo useradd -m -g users -s /bin/bash $user_name 
-#$sudo $(echo "$user_name:$user_pass" | chpasswd)
-echo "$user_name:$user_pass" | $sudo /usr/sbin/chpasswd
-
-# Initialize network configuration
-source $DIR/network-config.sh $hostname
-
-# Install xSession
+# xSession
 $install_cmd i3
 $install_cmd xorg
 
@@ -102,9 +119,11 @@ $install_cmd rsync	# Folder sync
 $install_cmd samba	# Windows shares
 $install_cmd sshfs	# SSH mounts
 $install_cmd sudo	# Substitute user do
+$install_cmd tmux	# Substitute user do
 $install_cmd vim	# Text editor
 $install_cmd wget	# FTP application
 $install_cmd xrandr	# Monitor configuration
+$install_cmd zsh	# ZSh
 
 # xSession tools
 $install_cmd arandr		# Monitor configuration
@@ -123,24 +142,39 @@ $install_cmd virtualbox		# System virtualization
 $install_cmd vlc		# Media player
 $install_cmd xautolock		# Session lockout
 
+##
 # Configuration
+##
 
 # Git
 $sudo git config --global user.email ${user_email}
 $sudo git config --global user.name ${user_full}
 
+# Grub
+$sudo grub-install --target=i386-pc ${diskpath}
+$sudo grub-mkconfig -o /boot/grub/grub.cfg
+
 # Linux repository
 mkdir /mnt/home/$user_name/documents
 mkdir /mnt/home/$user_name/documents/development
-$sudo git clone https://github.com/n0v1c3/linux.git /mnt/home/$user_name/documents/development
+$sudo git clone https://github.com/n0v1c3/linux.git /home/$user_name/documents/development
+
+# Root
+echo "root:$root_pass" | $sudo /usr/sbin/chpasswd
 
 # SLiM
 $sudo systemctl enable slim.service
 ln -s /usr/bin/slimlock /mnt/usr/local/bin/xflock4
 
-# Virtualbox - Guest
+# User
+$sudo useradd -m -g users -s /bin/bash $user_name 
+echo "$user_name:$user_pass" | $sudo /usr/sbin/chpasswd
+$sudo chown -R "$user_name:root" /home/$user_name
+
+# Virtualbox guest
 $install_cmd virtualbox-guest-modules-arch
 echo -e "vboxguest\nvboxsf\nvboxvideo" > /mnt/etc/modules-load.d/virtualbox.conf
 
 # Xinit
 echo "exec i3" > /mnt/root/.xinitrc
+echo "exec i3" > /mnt/home/$user_name/.xinitrc

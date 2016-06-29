cc28f732a9a545cef4537821d219d6036fd91830
Travis Gall <travis.gall@gmail.com>
2016-06-28 20:23:00 -0600

    install: enable all current features

--- tools/install.sh
+++ tools/install.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-##
-# Directories
-##
-
-# Current directory
-CUR="$(pwd)"
-
 # Script directory
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
-
-##
-# Functions
-##
-
-# Copy and date-stamp a given file
-function backup() {
-	cp $1 $1.backup-$(date +%y%m%d-%H%M%S-%N)
-}
 
 # TODO - Not Used
 function isLink() {
@@ -34,67 +18,86 @@ function isLink() {
 	return false
 }
 
-##
-# Symlinks
-##
-
-# List all dotfiles
-files=$(find $DIR -type f -name "*" | egrep "$DIR/config/")
-
-# Loop through all dotfiles contained in this repository
-for file in $files
-do
-	# File to be copied ($src) and destination ($dst)
-	dst=${file#*$DIR/config}
-	src=$file
+function getDst() 
+{
+	dst=${1#*$DIR/config}
 
 	# Adjust $dst path for current user's home directory
-	if ( [[ $dst == */home/* ]] )
+	if ( [[ $dst == /home/* ]] )
 	then
 		# Add current user's home path and strip ./home/* from $dst
 		dst=$HOME/${dst#*/*/}
 	fi
-
-	# Backup existing dotfile at $dst in place with timestamp
-	backup $dst
-
-	# Create symlink
-	ln -sf $src $dst
-done
-
-links=$(find -L $DIR -xtype l -name "*" | egrep "/config/")
-for link in $links
-do
-	# Destination path
-	dst=${link#*$DIR/config}
-
-	# Adjust $dst path for current user's home directory
-	if ( [[ $dst == */home/* ]] )
-	then
-		# Add current user's home path and strip ./home/* from $dst
-		dst=$HOME/${dst#/home/*}
-	fi
-
-	# Source path
-	src=$(stat -c %N $link)
-	src=${src#*->\ ‘}
-	src=$DIR/${src%*’}
 	
-	# Create symlink to directory
-	rm $dst
-	ln -s $link $dst
-done
+	echo $dst
+}
 
-##
-# Scripts
-##
+# List all config dotfiles
+files=$(find $DIR -type f -name "*" | egrep "$DIR/config/")
 
-# List all .sh scripts
-files=$(find $DIR -type f -name "*.sh" | egrep -v "$DIR/.git|$DIR/tools/")
+# List all dir dotfiles
+links=$(find -L $DIR -xtype l -name "*" | egrep "/config/")
 
-# Loop through all installation scripts in this repository
-for file in $files
+while test $# -gt 0
 do
-	# Run current $script
-	#sh $file
+	case "$1" in
+		-h|--help)
+			echo "Script: $DIR/tools/install.sh"
+			echo "-h, --help	show brief help"
+			echo "-b, --backup	backup all dotfiles"
+			echo "-l, --links	link all dotfiles and dotfolders"
+			echo "-s, --scripts	run all scripts in repository"
+			;;
+		-b|--backup)
+			# Backup existing dotfiles in place with timestamp
+			for file in $files
+			do
+				dst=$(getDst $file)
+				cp $dst $dst.backup-$(date +%y%m%d-%H%M%S-%N)
+			done
+			;;
+
+		-l|--link)
+			# Link all confg dotfiles
+			for file in $files
+			do
+				dst=$(getDst $file)
+				ln -sf $file $dst
+			done
+
+			# Link all directory dotfolders
+			for link in $links
+			do
+				# Destination path
+				dst=$(getDst $link)
+
+				# Source path
+				src=$(stat -c %N $link)
+				src=${src#*->\ ‘}
+				src=$DIR/${src%*’}
+
+				# Create symlink to directory
+				rm $dst
+				ln -s $link $dst
+			done
+			;;
+
+		-s|--scripts)
+			# List all .sh scripts
+			files=$(find $DIR -type f -name "*.sh" | egrep -v "$DIR/.git|$DIR/tools/")
+
+			# Loop through all installation scripts in this repository
+			for file in $files ; do sh $file ; done
+			;;
+
+		*)
+			break
+			;;
+	esac
+
+	# Shift $#
+	shift
 done
+
+# Finished no errors
+exit 0

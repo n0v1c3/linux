51a77123643164c8a884e38e2631eba7d6b47311
Travis Gall <travis.gall@gmail.com>
2016-07-16 16:32:58 -0600

    install: add mirror ranking

--- scripts/install/linux.sh
+++ scripts/install/linux.sh
@@ -73,6 +73,11 @@ mount ${diskpath}3 /mnt
 # Base
 ##
 
+# Rank mirrors by speed
+$sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+$sudo sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+$sudo rankmirrors -n 16 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+
 # Kernel
 $base_install
 

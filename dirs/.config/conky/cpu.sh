11fa931790fcb2875a3bb856b7be7fa00401936c
Travis Gall <travis.gall@gmail.com>
2016-07-18 10:10:38 -0600

    conky: add average load and missing processor core

--- dirs/.config/conky/cpu.sh
+++ dirs/.config/conky/cpu.sh
@@ -8,7 +8,7 @@ column_25=185
 echo   "+------------+-----------------+-----------------+-----------------+"
 printf "| %-10s | %-15s | %-15s | %-15s |\n" "CPU" "BAR GRAPH" "USAGE (%)" "T"
 echo   "+------------+-----------------+-----------------+-----------------+"
-for i in `seq 0  $(( $cpu_count-1 ))`
+for i in `seq 1  $(( $cpu_count ))`
 do
 	printf "| %-10s | %-15s | %-15s | %15s |\n" "cpu$i" "\${cpu cpu$i} %\${goto $column_25}" "\${cpubar cpu$i 10,90}" "\${cpugraph cpu$i 10,90}"
 done

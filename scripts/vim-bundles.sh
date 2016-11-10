462db20886334dbe6f21da6f5f4ae5dbcc815a0b
Travis Gall <travis.gall@gmail.com>
2016-11-09 18:22:07 -0700

    vim: clean-up bundle install script

--- scripts/vim-bundles.sh
+++ scripts/vim-bundles.sh
@@ -2,13 +2,16 @@
 
 # Name: vim-bundles.sh
 # Description: Clone and update desired bundles
+
 # TODO [161017] - This should be included with a dotfiles installer
 
 function gitHubBundle {
 # Directory path for plugin relative to ~/.vim/bundle
 dir=${1#*/}
 
-if [ ! -d $HOME/.vim/bundle/$dir ]; then
+# Confirm bundle directory
+if [ ! -d $HOME/.vim/bundle/$dir ]
+then
     # Directory does not exist, clone repository from github
     git clone "https://github.com/$1.git" ~/.vim/bundle/$dir
 else
@@ -24,6 +27,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 # Copy latest copy of pathogen from tpo.pe
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+# List of desired bundles
 declare -a gitHubList=(
 "scrooloose/nerdcommenter"
 "scrooloose/nerdtree"
@@ -31,6 +35,9 @@ declare -a gitHubList=(
 "dhruvasagar/vim-table-mode"
 )
 
-for i in ${gitHubList[@]}; do
+# Loop through each bundle
+for i in ${gitHubList[@]}
+do
+    # Download and install current bundle
     gitHubBundle "$i"
 done

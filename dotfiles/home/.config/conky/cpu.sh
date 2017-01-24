4f270e857572ed46795f92bcbf25052ea2b71a04
Travis Gall <travis.gall@gmail.com>
2017-01-23 21:37:36 -0700

    dotfiles: remove dotfiles/dirs folder

--- dotfiles/home/.config/conky/cpu.sh
+++ dotfiles/home/.config/conky/cpu.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 cpu_count=$(cat /proc/cpuinfo | grep processor | wc -l)
-table_width=407
+table_width=340
 graph_offset=20
-column_25=185
+column_25=155
 
 echo   "+------------+-----------------+-----------------+-----------------+"
 printf "| %-10s | %-15s | %-15s | %-15s |\n" "CPU" "BAR GRAPH" "USAGE (%)" "T"
 echo   "+------------+-----------------+-----------------+-----------------+"
 for i in `seq 1  $(( $cpu_count ))`
 do
-	printf "| %-10s | %-15s | %-15s | %15s |\n" "cpu$i" "\${cpu cpu$i} %\${goto $column_25}" "\${cpubar cpu$i 10,90}" "\${cpugraph cpu$i 10,90}"
+	printf "| %-10s | %-15s | %-15s | %15s |\n" "cpu$i" "\${cpu cpu$i} %\${goto $column_25}" "\${cpubar cpu$i 10,75}" "\${cpugraph cpu$i 10,75}"
 done
 echo   "+------------+-----------------+-----------------+-----------------+"
 echo   "|\${goto $table_width}|"
 echo   "|\${goto $table_width}|"
-echo   "\${goto 14}\${voffset -28}\${cpugraph 22,$(( $table_width-$graph_offset ))}\${goto $table_width}"
+echo   "\${goto 14}\${voffset -20}\${cpugraph 15,$(( $table_width-$graph_offset ))}\${goto $table_width}"
 echo   "\${voffset -5}+------------+-----------------+-----------------+-----------------+"

09e8c4f388ae4729f77eaaba57a508d048ba3fb6
Travis Gall <travis.gall@gmail.com>
2017-11-24 00:57:31 -0700

    backups: clean-up script

--- scripts/install/backup.sh
+++ scripts/install/backup.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
 # Name: backup.sh
-# Description: 
+# Description:
 
-# =======
-# Backups
-# =======
-
+# TODO-TJG [171114] - Clean-up method required
 # Local backup (no delete)
-sudo rsync --archive /home /backups/localhost
+sudo rsync --info=progress2 --archive /home /backups/localhost
 
 # Network backup (with delete) when connected
-if [ "$(mount | grep /mnt/tjg/dlink)" ]
-then
-    sudo rsync --archive --delete -e ssh /home /mnt/tjg/dlink/backups/$(hostname)
+if [ "$(mount | grep /mnt/tjg/dlink)" ]; then
+    sudo rsync --info=progress2 --delete -a -e ssh /home /mnt/tjg/dlink/backups/"$(hostname)"
 fi
-
-# ====
-# Exit
-# ====
-
-# Successful execution
 exit 0

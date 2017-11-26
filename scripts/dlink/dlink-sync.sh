ca36221e372b4873d7a7473a89d7b981a88eb7c2
Travis Gall <travis.gall@gmail.com>
2017-11-25 23:58:13 -0700

    remove old files

--- scripts/dlink/dlink-sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# TODO-TJG [171028] - Add clean-up commands to remove the crap from Documents
-rsync -avz -e ssh --info=progress2 --delete ~/Documents/ tjg-dlink:/ffp/home/"$(whoami)"/"$(hostname)"/Documents

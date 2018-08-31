de2e8f966da49e98fc99821efc5150b49857de1a
Travis Gall <travis.gall@gmail.com>
2018-08-30 20:22:05 -0600

    cron: comment out miner backup

--- cron/backup.sh
+++ cron/backup.sh
@@ -3,6 +3,6 @@
 # Name: backup.sh
 # Description:
 
-sudo mount /mnt/tjg/mine && sudo rsync -a --stats --delete /home /mnt/tjg/mine/back/"$(hostname)"
+# sudo mount /mnt/tjg/mine && sudo rsync -a --stats --delete /home /mnt/tjg/mine/back/"$(hostname)"
 
 exit 0

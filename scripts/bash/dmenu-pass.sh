97e9d69c2d36f843a4a8e7fde261f24f0e956dfa
Travis Gall <travis.gall@gmail.com>
2019-12-12 04:52:01 +0000

    LNUX: dmenu pass first line onle

--- scripts/bash/dmenu-pass.sh
+++ scripts/bash/dmenu-pass.sh
@@ -14,6 +14,6 @@ password_files=("${password_files[@]/$password_dir/}")
 
 password=$(printf '%s\n' "${password_files[@]}" | dmenu -i -b -p '   ' -fn 'DejaVu Sans Mono:regular:pixelsize=18' -nf '#4794ac' -nb '#0d0e11' -sb '#367c84' -sf '#0d0e11')
 
-pass -c "$password"
+pass -c "$password" | head -n 1
 
 exit 0

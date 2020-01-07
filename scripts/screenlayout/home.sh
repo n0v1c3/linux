9ddfd2db26f3eb617e3bcec4a632d9255b394234
Travis Gall <travis.gall@gmail.com>
2020-01-07 15:17:30 -0700

    LNUX-76: screenlayout has had 'output' names fixed

--- scripts/screenlayout/home.sh
+++ scripts/screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS-1 --primary --mode 1366x768 --pos 1600x432 --rotate normal --output VGA-1 --mode 1600x1200 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output HDMI-3 --off --output DP-2 --off --output DP-3 --off
+xrandr --output LVDS1 --mode 1366x768 --pos 1680x0 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VGA1 --mode 1680x1050 --pos 0x0 --rotate normal --output VIRTUAL1 --off
--- scripts/screenlayout/norm.sh
+++ scripts/screenlayout/norm.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output VGA-1 --off --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output HDMI-3 --off --output DP-2 --off --output DP-3 --off
+xrandr --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VGA1 --off --output VIRTUAL1 --off
